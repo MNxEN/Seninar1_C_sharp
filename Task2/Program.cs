dff752d6d99ba26c64b9819b171d25844cfee43e
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-09 23:32:18 +0500

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] week = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Введите номер дня недели: ");
+string weekday = Console.ReadLine();
+int dayNumber = Convert.ToInt32(weekday);
+if (dayNumber < 0 || dayNumber > 6)
+{
+    Console.WriteLine("Нет такого дня недели!");
+}
+else
+{
+    Console.WriteLine(week[dayNumber - 1]);
+}
+
+
+
