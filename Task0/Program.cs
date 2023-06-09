e9afd6305c09b93c45dcc0091d361199cf2d0a63
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-09 23:12:02 +0500

    Add Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter number =>");                           //вывод приграшения на экран
+string inputedString = Console.ReadLine();                   //ввод пользователя (строка)  
+int number = Convert.ToInt32(inputedString);                   //Преобразование строки в число
+Console.WriteLine($"You enter number: {number}!");          //Вывод введенного значения
+
+int square = number*number;                                  //Расчет квадрата числа   
+Console.WriteLine($"Square number {number} equals {square}");      //вывод результата
