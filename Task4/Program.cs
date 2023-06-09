3495674e346176bbbc60c44e792acff7ef28a04d
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-10 00:14:15 +0500

    Add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string strNumber = Console.ReadLine();
+int number1 = Convert.ToInt32(strNumber);
+int number2 = -number1;
+string result="";
+while(number2 <= number1)
+{
+    //Console.Write($"{number2} ");
+    result = ($"{result} {number2}");
+    number2++;
+};
+Console.WriteLine(result);
