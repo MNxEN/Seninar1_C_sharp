dbf163f7ba0e31dc68cbae2299d8081771399bc0
Mikhail Chekushkin <rbmihey@mail.ru>
2023-06-09 23:12:43 +0500

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число №1: ");     //Приглашение ввода 
+string firstStr = Console.ReadLine();
+Console.Write("Введите число №2: ");
+string secondStr = Console.ReadLine();
+
+int firstNumber = Convert.ToInt32(firstStr);
+int secondNumber = Convert.ToInt32(secondStr);
+
+int square = firstNumber * firstNumber;
+
+if (secondNumber == square) 
+    {
+        Console.WriteLine($"Второе число ({secondStr}) является квадратом первого числа ({firstStr})");
+    }
+else
+    {
+        Console.WriteLine($"Второе число ({secondStr}) не является квадратом первого числа ({firstStr})");
+    }
